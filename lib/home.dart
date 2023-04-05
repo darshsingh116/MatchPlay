838e6f4668ca0bec0e4f35286557873e2dbb12e5
unknown <sallygreanaud@gmail.com>
2023-04-05 18:27:31 +0530

    background

--- lib/home.dart
+++ lib/home.dart
@@ -7,53 +7,63 @@ class home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView(
-          children: <Widget>[
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/new.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: ListView(
+              children: <Widget>[
 
 
-            SizedBox(
-              width: 200.0,
-              height: 50.0,
-            ),
+                SizedBox(
+                  width: 200.0,
+                  height: 50.0,
+                ),
 
-            Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  'Home Page',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 60),
-                )),
-            SizedBox(
-              width: 200.0,
-              height: 100.0,
-            ),
-            ElevatedButton(onPressed: (){
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const MyApp()),
-              );
-            },
-                child: Text("Discover Page")),
+                Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.all(10),
+                    child: const Text(
+                      'MatchPlay!',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 60),
+                    )),
+                SizedBox(
+                  width: 200.0,
+                  height: 200.0,
+                ),
+                ElevatedButton(onPressed: (){
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const MyApp()),
+                  );
+                },
+                    child: Text("Discover Page")),
 
-            SizedBox(
-              width: 200.0,
-              height: 100.0,
-            ),
+                SizedBox(
+                  width: 200.0,
+                  height: 50.0,
+                ),
 
-            ElevatedButton(onPressed: (){
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => Profile()),
-              );
-            },
-                child: Text("Profile page"))
+                ElevatedButton(onPressed: (){
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Profile()),
+                  );
+                },
+                    child: Text("Profile page"))
 
 //
-          ],
-        ));
+              ],
+            )),
+      ),
+    );
   }
 }
--- lib/login.dart
+++ lib/login.dart
@@ -12,7 +12,6 @@ class signup extends StatelessWidget {
     return MaterialApp(
       title: _title,
       home: Scaffold(
-        backgroundColor: Colors.grey[700],
         body: const MyStatefulWidget(),
       ),
     );
@@ -32,108 +31,119 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView(
-          children: <Widget>[
+    return Scaffold(
+      body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/1.jpg"),
+              fit: BoxFit.cover,
 
-
-            SizedBox(
-              width: 200.0,
-              height: 50.0,
             ),
-
-            Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: Text(
-                  'Log In',
-                  style: TextStyle(
-                    color: Colors.white,
-                      fontFamily: "Poppins",
-                      fontSize: 60),
-                )),
-            SizedBox(
-              width: 200.0,
-              height: 100.0,
-            ),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                controller: nameController,
-                decoration: const InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(),
-                  labelText: 'User Name',
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-              child: TextField(
-                obscureText: true,
-                controller: passwordController,
-                decoration: const InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                ),
-              ),
-            ),
-            TextButton(
-              onPressed: () {
-                //forgot password screen
-              },
-              child: const Text('Forgot Password',),
-            ),
-            Container(
-                height: 50,
-                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      primary: Color(11714579),
-                      textStyle: TextStyle(
-                          fontSize: 30,
-                          fontFamily: "Poppins",
-                          fontWeight: FontWeight.bold)),
-                  child: Text('Login'),
-                  onPressed: () {
-
-                    if(nameController.text=="blackbuck" && passwordController.text=="12345"){
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const home()),
-                      );
-                    }
-
-
-
-                    print(nameController.text);
-                    print(passwordController.text);
-                  },
-                )
-            ),
-            Row(
+          ),
+        child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: ListView(
               children: <Widget>[
-                const Text('Don\'t have an account?',
-                style: TextStyle(color: Colors.white),),
-                TextButton(
-                  child: const Text(
-                    'Sign up',
-                    style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20),
+
+
+                SizedBox(
+                  width: 200.0,
+                  height: 50.0,
+                ),
+
+                Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      'Log In',
+                      style: TextStyle(
+                        color: Colors.white,
+                          fontFamily: "Poppins",
+                          fontSize: 60),
+                    )),
+                SizedBox(
+                  width: 200.0,
+                  height: 100.0,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  child: TextField(
+                    controller: nameController,
+                    decoration: const InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(),
+                      labelText: 'User Name',
+                    ),
                   ),
+                ),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextField(
+                    obscureText: true,
+                    controller: passwordController,
+                    decoration: const InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                  ),
+                ),
+                TextButton(
                   onPressed: () {
-                    //signup
+                    //forgot password screen
                   },
-                )
+                  child: const Text('Forgot Password',),
+                ),
+                Container(
+                    height: 50,
+                    padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: Color(11714579),
+                          textStyle: TextStyle(
+                              fontSize: 30,
+                              fontFamily: "Poppins",
+                              fontWeight: FontWeight.bold)),
+                      child: Text('Login'),
+                      onPressed: () {
+
+                        if(nameController.text=="blackbuck" && passwordController.text=="12345"){
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const home()),
+                          );
+                        }
+
+
+
+                        print(nameController.text);
+                        print(passwordController.text);
+                      },
+                    )
+                ),
+                Row(
+                  children: <Widget>[
+                    const Text('Don\'t have an account?',
+                    style: TextStyle(color: Colors.white),),
+                    TextButton(
+                      child: const Text(
+                        'Sign up',
+                        style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20),
+                      ),
+                      onPressed: () {
+                        //signup
+                      },
+                    )
+                  ],
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ),
               ],
-              mainAxisAlignment: MainAxisAlignment.center,
-            ),
-          ],
-        ));
+            )),
+      ),
+    );
   }
 }
